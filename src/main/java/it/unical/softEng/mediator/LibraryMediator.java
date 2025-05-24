ff6f1bfda746779bd76fb6b740d03ff19f365f7f
thecoder <leonardo.npl.02@gmail.com>
2025-05-24 10:15:24 +0200

    Implemented mediator

---
+++ src/main/java/it/unical/softEng/mediator/LibraryMediator.java
@@ -0,0 +1,13 @@
+package it.unical.softEng.mediator;
+
+public class LibraryMediator implements Mediator {
+
+  @Override
+  public void notify(MediatorComponent sender) {
+
+  }
+
+  private void reactToDB() {
+
+  }
+}
