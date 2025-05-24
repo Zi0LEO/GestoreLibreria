c645e6f47d0f5c2b6a65d281a39a96dccecdd3cc
thecoder <leonardo.npl.02@gmail.com>
2025-05-24 12:38:59 +0200

    Add GsonFacade for JSON serialization and Gson extras dependency

---
+++ src/main/java/it/unical/softEng/facade/GsonFacade.java
@@ -0,0 +1,47 @@
+package it.unical.softEng.facade;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
+import it.unical.softEng.composite.Book;
+import it.unical.softEng.composite.Collection;
+import it.unical.softEng.composite.ElementComposite;
+
+import java.io.*;
+
+public class GsonFacade {
+  private final Gson gson;
+  private GsonPersistentFacade stateManager;
+  private String directoryPath;
+
+  public GsonFacade() {
+    RuntimeTypeAdapterFactory<ElementComposite> factory =
+        RuntimeTypeAdapterFactory.of(ElementComposite.class, "type").
+            registerSubtype(Book.class, "book").
+            registerSubtype(Collection.class, "collection");
+    this.gson = new GsonBuilder().
+        registerTypeAdapterFactory(factory).
+        setPrettyPrinting().
+        create();
+  }
+
+  public boolean save(ElementComposite element) {
+    File output = new File(directoryPath, element.getId()+".json");
+    try(FileWriter writer = new FileWriter(output))
+    {
+      gson.toJson(element, writer);
+    } catch (IOException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public ElementComposite read(String id){
+    File output = new File(directoryPath, id+".json");
+    try(FileReader reader = new FileReader(output)){
+      return gson.fromJson(reader, ElementComposite.class);
+    } catch (IOException e) {
+      return null;
+    }
+  }
+}
