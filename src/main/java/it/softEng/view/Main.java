152e6579e26d0e82ffb4a9383ca2f266130d925b
thecoder <leonardo.npl.02@gmail.com>
2025-05-30 11:34:56 +0200

    Switch from fxml to regular JavaFX

--- src/main/java/it/softEng/view/Main.java
+++ src/main/java/it/softEng/view/Main.java
@@ -1,30 +1,54 @@
 package it.softEng.view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
-
 public class Main extends Application {
+  GuiFactory factory;
+
   @Override
   public void start(Stage stage){
-    try {
-      URL fxmlUrl = getClass().getResource("/it/softEng/view/Main.fxml");
-      Parent root = FXMLLoader.load(fxmlUrl);
-      System.out.println("FXML URL: " + fxmlUrl);
-      Scene scene = new Scene(root);
-      stage.setTitle("My JavaFX App");
-      stage.setScene(scene);
-      stage.setHeight(450);
-      stage.setWidth(800);
-      stage.show();
-    }catch(IOException e){
-      e.printStackTrace();
-    }
+
+    this.factory = new MinecraftFactory();
+    double WIDTH = stage.getWidth();
+    double HEIGHT = stage.getHeight();
+
+    StackPane root = new StackPane();
+    BorderPane mainContent = new BorderPane();
+    VBox vbox = new VBox();
+
+    Scene scene = new Scene(root);
+
+    Button button1 = factory.createButton("test1");
+    Button button2 = factory.createButton("test2");
+    Media background = factory.createBackground();
+
+    MediaPlayer backgroundPlayer = new MediaPlayer(background);
+    MediaView mediaView = new MediaView(backgroundPlayer);
+    backgroundPlayer.setAutoPlay(true);
+    backgroundPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+    mediaView.prefHeight(HEIGHT);
+    root.getChildren().add(mediaView);
+
+    vbox.getChildren().addAll(button1, button2);
+    vbox.setSpacing(10);
+    vbox.prefWidthProperty().bind(mainContent.widthProperty().multiply(0.15));
+    vbox.prefHeightProperty().bind(mainContent.heightProperty());
+    vbox.setStyle("-fx-background-color: rgba(0,100,100,0.59);");
+    mainContent.setLeft(vbox);
+    root.getChildren().add(mainContent);
+
+    stage.setTitle("My JavaFX App");
+    stage.setScene(scene);
+    stage.show();
   }
 
   public static void main(String[] args) {
