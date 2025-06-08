2d6f644e6443bec80c7ae89b095b2260a37e3afb
thecoder <leonardo.npl.02@gmail.com>
2025-06-09 00:49:33 +0200

    Refactor GUI structure, fixed resizing problems

--- src/main/java/it/softEng/view/Main.java
+++ src/main/java/it/softEng/view/Main.java
@@ -4,52 +4,23 @@ import it.softEng.view.abstractFactory.GuiFactory;
 import it.softEng.view.abstractFactory.MinecraftFactory;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   GuiFactory factory;
 
   @Override
+  public void init() throws Exception {
+    super.init();
+  }
+
+  @Override
   public void start(Stage stage){
 
     this.factory = MinecraftFactory.getInstance();
-    double WIDTH = stage.getWidth();
-    double HEIGHT = stage.getHeight();
-
-    stage.setMinWidth(500);
-    stage.setMinHeight(250);
-    StackPane root = new StackPane();
-    BorderPane mainContent = new BorderPane();
-    VBox vbox = new VBox();
-
-    Scene scene = new Scene(root);
-    scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
-
-    Button button1 = factory.createButton("test1");
-    Button button2 = factory.createButton("test2");
-    Media background = factory.createBackground();
-
-    MediaPlayer backgroundPlayer = new MediaPlayer(background);
-    MediaView mediaView = new MediaView(backgroundPlayer);
-    backgroundPlayer.setAutoPlay(true);
-    backgroundPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-    mediaView.prefHeight(HEIGHT);
-    root.getChildren().add(mediaView);
-
-    vbox.getChildren().addAll(button1, button2);
-    vbox.setSpacing(10);
-    vbox.prefWidthProperty().bind(mainContent.widthProperty().multiply(0.15));
-    vbox.prefHeightProperty().bind(mainContent.heightProperty());
-    vbox.setStyle("-fx-background-color: rgba(0,100,100,0.59);");
-    mainContent.setLeft(vbox);
-    root.getChildren().add(mainContent);
+    Scene scene = this.factory.createMainScene(stage);
+    stage.setMinHeight(600);
+    stage.setMinWidth(800);
 
     stage.setTitle("My JavaFX App");
     stage.setScene(scene);
--- src/main/java/it/softEng/view/abstractFactory/MinecraftFactory.java
+++ src/main/java/it/softEng/view/abstractFactory/MinecraftFactory.java
@@ -1,8 +1,20 @@
 package it.softEng.view.abstractFactory;
 
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.Effect;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 
 public class MinecraftFactory implements GuiFactory {
@@ -17,6 +29,26 @@ public class MinecraftFactory implements GuiFactory {
   }
 
   @Override
+  public Scene createMainScene(Stage stage) {
+
+    StackPane root = new StackPane();
+    Scene scene = new Scene(root);
+
+    Button button1 = createButton("test1");
+    Button button2 = createButton("test2");
+    MediaView background = createBackground(stage);
+    root.getChildren().add(background);
+
+    Pane sidebar = createSidebar(createBackground(stage), button1, button2);
+    root.getChildren().add(sidebar);
+
+    StackPane.setAlignment(sidebar, javafx.geometry.Pos.CENTER_LEFT);
+
+    scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+    return scene;
+  }
+
+  @Override
   public Button createButton(String text) {
     return new MinecraftButton(text);
   }
@@ -27,7 +59,39 @@ public class MinecraftFactory implements GuiFactory {
   }
 
   @Override
-  public Media createBackground() {
-    return new Media(getClass().getResource("/video/minecraft2.mp4").toExternalForm());
+  public MediaView createBackground(Stage stage) {
+    Media video = new Media(getClass().getResource("/video/minecraft2.mp4").toExternalForm());
+    MediaPlayer backgroundPlayer = new MediaPlayer(video);
+    backgroundPlayer.setAutoPlay(true);
+    backgroundPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+    MediaView background = new MediaView(backgroundPlayer);
+    background.fitWidthProperty().bind(stage.widthProperty());
+    background.fitHeightProperty().bind(stage.heightProperty());
+    background.setPreserveRatio(false);
+    return background;
+  }
+
+  private static final Effect BLUR = new BoxBlur(15, 15, 3);
+
+  private Pane createSidebar(MediaView background, Node...nodes){
+    StackPane sidebar = new StackPane();
+    Rectangle backgroundContainer = new Rectangle();
+    VBox vbox = new VBox();
+
+    backgroundContainer.setWidth(200);
+    sidebar.setPrefWidth(200);
+    Platform.runLater(() -> {
+      sidebar.prefHeightProperty().bind(sidebar.getScene().heightProperty());
+      backgroundContainer.heightProperty().bind(sidebar.getScene().heightProperty());
+    });
+    background.setClip(backgroundContainer);
+    background.setEffect(BLUR);
+    sidebar.getChildren().add(background);
+
+    vbox.getChildren().addAll(nodes);
+    vbox.getStyleClass().add("minecraft-sidebar");
+    vbox.setSpacing(10);
+    sidebar.getChildren().add(vbox);
+    return sidebar;
   }
 }
